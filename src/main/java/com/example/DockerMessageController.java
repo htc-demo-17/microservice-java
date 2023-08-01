3ea4d7f4684d49f517ddb525f4553fdde950f147
omichels <omichels@users.noreply.github.com>
2023-08-01 16:08:30 +0200

    Update DockerMessageController.java

--- src/main/java/com/example/DockerMessageController.java
+++ src/main/java/com/example/DockerMessageController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DockerMessageController {
     @GetMapping("/messages")
     public String getMessage() {
-        return "Hello from Docker!";
+        return "Hello from Docker to Michael!";
     }
 }
